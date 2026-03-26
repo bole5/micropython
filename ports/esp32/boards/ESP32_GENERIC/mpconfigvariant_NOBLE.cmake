5f76f251fe54878742f1c5cfa998394fc73d4987
bole5 <bole5@yahoo.com>
2026-03-26 19:49:44 +0100

    Update mpconfigvariant_NOBLE.cmake
    Signed-off-by: bole5 <bole5@yahoo.com>

--- ports/esp32/boards/ESP32_GENERIC/mpconfigvariant_NOBLE.cmake
+++ ports/esp32/boards/ESP32_GENERIC/mpconfigvariant_NOBLE.cmake
@@ -1,3 +1,8 @@
+# Force-disable Bluetooth at cmake level
+set(MICROPY_PY_BLUETOOTH 0 CACHE BOOL "" FORCE)
+set(MICROPY_BLUETOOTH_NIMBLE 0 CACHE BOOL "" FORCE)
+
+# Point to a sdkconfig that disables BLE in ESP-IDF
 set(SDKCONFIG_DEFAULTS
     boards/sdkconfig.base
     boards/ESP32_GENERIC/sdkconfig.noble
